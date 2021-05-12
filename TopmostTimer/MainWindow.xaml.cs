27aaeeb513ed4bcb3608bbf2126a734e08b3e78b
James Cho <chojames500@gmail.com>
2021-05-12 23:46:14 +0900

    Toolbar added; resize, minimize, and close window features added

--- TopmostTimer/MainWindow.xaml.cs
+++ TopmostTimer/MainWindow.xaml.cs
@@ -21,19 +21,54 @@ namespace TopmostTimer
     /// </summary>
     public partial class MainWindow : Window
     {
+        private LinkedList<(double height, double width)> _windowSizes = new LinkedList<(double, double)>();
+        private LinkedListNode<(double height, double width)> _currentSize;
+
         public MainWindow(MainViewModel mainViewModel)
         {
             InitializeComponent();
-
+            
             DataContext = mainViewModel;
+
+            this.InitializeWindowSize();
         }
 
+        private void InitializeWindowSize()
+        {
+            var smallWindow = new LinkedListNode<(double, double)>((175, 210));
+            var bigWindow = new LinkedListNode<(double, double)>((this.Height, this.Width));
+
+            _windowSizes.AddLast(smallWindow);
+            _windowSizes.AddLast(bigWindow);
+            _currentSize = _windowSizes.Last;
+
+            this.Height = _currentSize.Value.height;
+            this.Width = _currentSize.Value.width;
+        }
 
         private void DragOver_MouseDown(object sender, MouseButtonEventArgs e)
         {
             if (e.ChangedButton == MouseButton.Left)
                 DragMove();
 
+        }
+
+        private void CollapseWindow_Click(object sender, RoutedEventArgs e)
+        {
+            SystemCommands.MinimizeWindow(this);
+        }
+
+        private void CloseApp_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void ResizeWindow_Click(object sender, RoutedEventArgs e)
+        {
+            _currentSize = _currentSize.Next == null ? _currentSize.Previous : _currentSize.Next;
+
+            this.Height = _currentSize.Value.height;
+            this.Width = _currentSize.Value.width;
         }
     }
 }
